950fdcf927883ceb51b270d47a81af5ca5b00981
JEVizcaya <jevizcaya@hotmail.com>
2025-01-16 16:49:31 +0100

    registro.php

---
+++ registro.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="post">
+    <label for="nombre">Nombre</label>
+    <input type="text" name="nombre" id="nombre" required>
+    <label for="email">email</label>
+    <input type="email" name="email" id="email" required>
+    <label for="password">Password</label>
+    <input type="password" name="password" id="password" required>
+    <input type="submit" value="Registrarse">
+</form>
+</body>
+</html>
