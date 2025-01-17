8fe5bb87b6adc664dc4edf523ab02733557558bc
JEVizcaya <jevizcaya@hotmail.com>
2025-01-17 15:30:38 +0100

    dar like 2

--- like.php
+++ like.php
@@ -1,13 +1,18 @@
 <?php
 session_start();
 if (!isset($_SESSION["idusuario"])){
-    header("Location=index.php");
+    header("Location: index.php");
     exit();
 }
 if(isset($_GET["idfoto"])){
-    try {
     include("conexiondb.php");
-    $sql=
-    }
+    $sql="INSERT INTO likes (idfoto, idusuario) VALUES (:idfoto, :idusuario)";
+    $stm=$conexion->prepare($sql);
+    $stm->bindParam(":idfoto", $_GET["idfoto"]);
+    $stm->bindParam(":idusuario", $_SESSION["idusuario"]);
+    $stm->execute();
+    header("Location: index.php");
+    exit();
 }
+
 ?>
